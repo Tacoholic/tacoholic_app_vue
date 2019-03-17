ca548ed9937583d3ed6c82cc6e3c1519726acc77
Tacoholic <solischristian@gmail.com>
2019-03-16 22:53:00 -0500

    adjusted the reviews, login, signup and logout routes

--- src/router.js
+++ src/router.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import RestaurantsIndex from './views/RestaurantsIndex.vue';
 import RestaurantsNew from './views/RestaurantsNew.vue';
-import RestaurantsShow from './views/RestaurantsShow.vue';
+import RestaurantsReview from "./views/RestaurantsReview.vue";
 import Signup from "./views/Signup.vue";
 import Login from "./views/Login.vue";
 import Logout from "./views/Logout.vue";
+import RestaurantsShow from './views/RestaurantsShow.vue';
 import About from "./views/About.vue";
-import RestaurantsReview from "./views/RestaurantsReview.vue";
+
 
 
 Vue.use(Router)
@@ -19,12 +20,13 @@ export default new Router({
   routes: [
     { path: '/restaurants', name: 'restaurant', component: RestaurantsIndex},
     { path: '/restaurants/new', name: 'restaurants-new', component: RestaurantsNew},
-    { path: '/restaurants/:id', name: 'restaurants-show', component: RestaurantsShow},
+    { path: '/restaurants/:id/reviews', name: 'review', component: RestaurantsReview},
     { path: '/restaurants/signup', name: 'signup', component: Signup},
     { path: '/restaurants/login', name: 'login', component: Login},
     { path: '/restaurants/logout', name: 'logout', component: Logout},
-    { path: '/restaurants/about', name: 'about', component: About},
-    { path: '/restaurants/:id/reviews', name: 'review', component: RestaurantsReview}
+    { path: '/restaurants/:id', name: 'restaurants-show', component: RestaurantsShow},
+    { path: '/restaurants/about', name: 'about', component: About}
+   
   ]
 })
 
